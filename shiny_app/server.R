3ec32dc22950322d73e64c0c5ba00b13ce466e1d
phoward38 <phoward38@gatech.edu>
2019-10-23 17:51:08 -0400

    Removed `source` b/c of module call

--- shiny_app/server.R
+++ shiny_app/server.R
@@ -4,7 +4,6 @@ server <- function(input, output, session) {
   session$userData$conn <- conn
   session$userData$db_trigger <- reactiveVal(0)
 
-  #source('server/01.1_s_car_table.R', local = TRUE)
   callModule(
     cars_table_module,
     "cars_table"
