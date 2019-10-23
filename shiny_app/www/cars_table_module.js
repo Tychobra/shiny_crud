07c48bb43896940b180003b5e741761cd3bf3e79
phoward38 <phoward38@gatech.edu>
2019-10-23 14:19:04 -0400

    Updated JS to account for using id instead of `row_num`

--- shiny_app/www/cars_table_module.js
+++ shiny_app/www/cars_table_module.js
@@ -4,12 +4,14 @@
 function cars_table_module_js(ns) {
 
   $(document).on("click", "#" + ns + "car_table .delete_btn", function() {
-    Shiny.setInputValue(ns + "car_row_to_delete", this.id, { priority: "event"});
+    Shiny.setInputValue(ns + "car_id_to_delete", this.id, { priority: "event"});
     $(this).tooltip('hide');
   });
 
+  debugger;
+
   $(document).on("click", "#" + ns + "car_table .edit_btn", function() {
-    Shiny.setInputValue(ns + "car_row_to_edit", this.id, { priority: "event"});
+    Shiny.setInputValue(ns + "car_id_to_edit", this.id, { priority: "event"});
     $(this).tooltip('hide');
   });
 }
