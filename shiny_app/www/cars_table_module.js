8bc60fd252177a65943a42fc7b8408fb1ef2559a
phoward38 <phoward38@gatech.edu>
2019-12-16 16:17:06 -0500

    Fixed edit/delete functionality

--- shiny_app/www/cars_table_module.js
+++ shiny_app/www/cars_table_module.js
@@ -4,12 +4,12 @@
 function cars_table_module_js(ns_prefix) {
 
   $(document).on("click", "#" + ns_prefix + "-car_table .delete_btn", function() {
-    Shiny.setInputValue(ns + "car_id_to_delete", this.id, { priority: "event"});
+    Shiny.setInputValue(ns_prefix + "-car_id_to_delete", this.id, { priority: "event"});
     $(this).tooltip('hide');
   });
 
   $(document).on("click", "#" + ns_prefix + "-car_table .edit_btn", function() {
-    Shiny.setInputValue(ns + "car_id_to_edit", this.id, { priority: "event"});
+    Shiny.setInputValue(ns_prefix + "-car_id_to_edit", this.id, { priority: "event"});
     $(this).tooltip('hide');
   });
 }
