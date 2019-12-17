e94675ef32f74a27ad34bbe8daefff04f4bf6594
merlinoa <andy.merlino@tychobra.com>
2019-12-17 11:08:26 -0500

    use table id rather than document in jQuery selectors

--- shiny_app/www/cars_table_module.js
+++ shiny_app/www/cars_table_module.js
@@ -3,12 +3,12 @@
 
 function cars_table_module_js(ns_prefix) {
 
-  $(document).on("click", "#" + ns_prefix + "-car_table .delete_btn", function() {
+  $("#" + ns_prefix + "-car_table").on("click", ".delete_btn", function() {
     Shiny.setInputValue(ns_prefix + "-car_id_to_delete", this.id, { priority: "event"});
     $(this).tooltip('hide');
   });
 
-  $(document).on("click", "#" + ns_prefix + "-car_table .edit_btn", function() {
+  $("#" + ns_prefix + "-car_table ").on("click", "#" + ns_prefix + ".edit_btn", function() {
     Shiny.setInputValue(ns_prefix + "-car_id_to_edit", this.id, { priority: "event"});
     $(this).tooltip('hide');
   });
