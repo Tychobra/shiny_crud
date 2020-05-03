c341984af6acf508bea59c4a011e0e8b83800655
merlinoa <andy.merlino@tychobra.com>
2020-05-03 12:59:31 -0400

    move user feedback observe into modal observe

--- traditional/shiny_app/R/car_edit_module.R
+++ traditional/shiny_app/R/car_edit_module.R
@@ -128,23 +128,27 @@ car_edit_module <- function(input, output, session, modal_title, car_to_edit, mo
         )
       )
     )
+
+    # Observe event for "Model" text input in Add/Edit Car Modal
+    # `shinyFeedback`
+    observeEvent(input$model, {
+      if (input$model == "") {
+        shinyFeedback::showFeedbackDanger(
+          "model",
+          text = "Must enter model of car!"
+        )
+        shinyjs::disable('submit')
+      } else {
+        shinyFeedback::hideFeedback("model")
+        shinyjs::enable('submit')
+      }
+    })
+
   })
 
-  # Observe event for "Model" text input in Add/Edit Car Modal
-  # `shinyFeedback`
-  observeEvent(input$model, {
 
-    if (input$model == "") {
-      shinyFeedback::showFeedbackDanger(
-        "model",
-        text = "Must enter model of car!"
-      )
-      shinyjs::disable('submit')
-    } else {
-      shinyFeedback::hideFeedback("model")
-      shinyjs::enable('submit')
-    }
-  })
+
+
 
   edit_car_dat <- reactive({
     hold <- car_to_edit()
