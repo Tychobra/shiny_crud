4acbef6f77400865d0f6595be538535be1266714
merlinoa <andy.merlino@tychobra.com>
2020-05-06 08:36:47 -0400

    ui title

--- 02_auditable/shiny_app/ui.R
+++ 02_auditable/shiny_app/ui.R
@@ -3,8 +3,8 @@ ui <- fluidPage(
   shinyjs::useShinyjs(),
   # Application Title
   titlePanel(
-    h1("Shiny CRUD Application", align = 'center'),
-    windowTitle = "Shiny CRUD Application"
+    h1("Shiny CRUD - Auditable", align = 'center'),
+    windowTitle = "Shiny CRUD - Auditable"
   ),
   cars_table_module_ui("cars_table")
 )
