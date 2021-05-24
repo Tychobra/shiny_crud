842dfb3a2db6828abd3491ee086059b469f5e608
Niccolo Salvini <niccolo.salvini27@gmail.com>
2021-05-24 23:50:18 +0200

    del a bunch of files not related to my proj

--- 03_traditional_polished/deploy.R
+++
@@ -1,6 +0,0 @@
-
-rsconnect::deployApp(
-  appDir = "shiny_app",
-  account = "tychobra",
-  appName = "crud_polished"
-)
--- 04_yssup_trackings/deploy.R
+++
@@ -1,8 +0,0 @@
-
-app_name <- config::get(file = "04_yssup_trackings/shiny_app/config.yml")$app_name
-rsconnect::deployApp(
-  appDir = "04_yssup_trackings/shiny_app",
-  account = "tychobra",
-  appName = app_name
-)
-
