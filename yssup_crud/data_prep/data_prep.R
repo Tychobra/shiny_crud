4efe03598f3d15166f2e652f686279c92c963a70
Niccolo Salvini <niccolo.salvini27@gmail.com>
2021-05-24 23:50:35 +0200

    updated direcdtoy name

---
+++ yssup_crud/data_prep/data_prep.R
@@ -0,0 +1,21 @@
+library(dplyr, quietly = T, warn.conflicts = F)
+library(tidyr, quietly = T, warn.conflicts = F)
+library(tibble, quietly = T ,warn.conflicts = F)
+library(here, quietly = T ,warn.conflicts = F)
+library(readxl, quietly = T ,warn.conflicts = F)
+library(janitor, quietly = T ,warn.conflicts = F)
+library(forcats, quietly = T ,warn.conflicts = F)
+
+yssup <- read_excel(here("04_yssup_trackings", "data_prep", "provided","yssup_data.xlsx")) %>%
+  clean_names() %>%
+  rename(tipette = x1) %>%
+  mutate(
+    across(everything(), ~ replace_na(.x, "niente")),
+    across(everything(), ~ as_factor(x = .x)),
+    across(everything(), ~ fct_recode(.x, scopata = "s")),
+    across(everything(), ~ fct_recode(.x, baciata = "b"))
+  )
+
+
+saveRDS(yssup, file = '04_yssup_trackings/data_prep/prepped/yssup.RDS')
+
---
+++ yssup_crud/shiny_app/ui.R
@@ -0,0 +1,12 @@
+fluidPage(
+  shinyFeedback::useShinyFeedback(),
+  shinyjs::useShinyjs(),
+  # Application Title
+  titlePanel(
+    h1("Explore Yssup records", align = 'center'),
+    windowTitle = "yssup database"
+  ),
+  yssup_table_module_ui("yssup_table")
+)
+
+## aggiungi dynamic plots 
